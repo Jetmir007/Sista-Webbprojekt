00136503dc117d9759b9cf8d515e62da1ed1a3b8
Jetmir007 <jeku07001@utb.vaxjo.se>
2025-04-23 09:17:56 +0200

    Sista webb

---
+++ script.js
@@ -0,0 +1,26 @@
+function ToggleD(){
+    let html = document.getElementsByTagName("html") 
+    html[0].classList.toggle("theme-dark")
+    document.getElementById("sol").style.display = "none"
+    document.getElementById("måne").style.display = "block"
+}
+
+function ToggleL(){
+    let html = document.getElementsByTagName("html") 
+    html[0].classList.toggle("theme-dark")
+    document.getElementById("sol").style.display = "block"
+    document.getElementById("måne").style.display = "none"
+}
+
+let drop = false
+
+function Nav(){
+    if(!drop){
+        document.getElementById("dropdown").style.display = "block"
+        drop = true
+    }
+    else{
+        document.getElementById("dropdown").style.display = "none"
+        drop = false
+    }
+}
